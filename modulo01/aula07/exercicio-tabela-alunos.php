92a34c7379f74d821f89def4ac2eb7a059468b8c
Daniel Attias <danielattias@live.com>
2022-06-08 20:34:59 -0400

    atualizacoes

--- modulo01/aula07/exercicio-tabela-alunos.php
+++ modulo01/aula07/exercicio-tabela-alunos.php
@@ -29,29 +29,37 @@ $alunos = [
 
 //var_dump($alunos);
 ?>
-<table border="1">
-    <thead>
-        <tr>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Telefone</th>
-            <th colspan="4">Notas</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-        <?php
-            foreach($alunos as $aluno) {
-                echo "<tr>
-                        <td>" . $aluno['nome'] . "</td>
-                        <td>" . $aluno['email'] . "</td>
-                        <td>" . $aluno['telefone'] . "</td>
-                        <td>" . $aluno['notas'][0] . "</td>
-                        <td>" . $aluno['notas'][1] . "</td>
-                        <td>" . $aluno['notas'][2] . "</td>
-                        <td>" . $aluno['notas'][3] . "</td>
-                    </tr>";
-            }
-        ?>
-    </tbody>
-</table>
+
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+<div class="container">
+    <h1 class="mt-5">Alunos</h1>
+    <hr>
+    <table class="table table-hover table-striped mt-5">
+        <thead class="table-dark">
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th colspan="4">Notas</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            <?php
+                foreach($alunos as $aluno) {
+                    echo "<tr>
+                            <td>" . $aluno['nome'] . "</td>
+                            <td>" . $aluno['email'] . "</td>
+                            <td>" . $aluno['telefone'] . "</td>
+                            <td>" . $aluno['notas'][0] . "</td>
+                            <td>" . $aluno['notas'][1] . "</td>
+                            <td>" . $aluno['notas'][2] . "</td>
+                            <td>" . $aluno['notas'][3] . "</td>
+                        </tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+</div>
