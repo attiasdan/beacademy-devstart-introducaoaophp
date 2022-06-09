b808ba7bec63c27fde58ec31eec0237b0bd99565
Daniel Attias <danielattias@live.com>
2022-06-08 21:23:51 -0400

    atualizações

---
+++ modulo01/aula08/get.php
@@ -0,0 +1,10 @@
+<?php
+
+if($_GET) {
+    echo $_GET['nome'] . "<br>";
+    echo $_GET['cidade'] . "<br>";
+    echo $_GET['cor'] . "<br>";
+}
+
+//http://localhost:8000/modulo01/aula08/get.php?nome=Daniel&cidade=Porto_Velho&cor=branco
+?>
