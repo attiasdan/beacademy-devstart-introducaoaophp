5538a0172c85808bd2aaf6ece3647add945a817d
Daniel Attias <danielattias@live.com>
2022-06-09 14:53:48 -0400

    atualizações

--- modulo01/funcoes/index.php
+++ modulo01/funcoes/index.php
@@ -2,8 +2,12 @@
 
 declare(strict_types=1);
 
-function soma(int $n1, int $n2) {
+function soma(float $n1, float $n2) {
     return $n1 + $n2;
+}
+
+function welcome(string $nome) {
+    return "Bem vindo {$nome}";
 }
 
 echo soma(10, 20);
@@ -11,5 +15,7 @@ echo soma(10, 20);
 echo PHP_EOL . '----------------' . PHP_EOL;
 
 echo soma(10, 90);
+
+echo welcome("Daniel");
 
 ?>
